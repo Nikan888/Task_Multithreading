0fbc7c63de279753aab63c9e3b9d4ea63f3aa011
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-07-22 04:32:35 +0300

    Task_Multithreading

---
+++ Task_Multithreading/GCDComputer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Multithreading
+{
+    public class GCDComputer
+    {
+        public static long FindMin(long a, long b)
+        {
+            return a > b ? b : a;
+        }
+
+        public static long FirstGCD(long a, long b)
+        {
+            long nod = 1L;
+
+            for (long i = a; i > 0; i--)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    nod = i;
+                    break;
+                }
+            }
+            return nod;
+        }
+
+        public static long SecondGCD(long a, long b)
+        {
+            long nod = 1L;
+
+            for (long i = FindMin(a, b); i > 0; i--)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    nod = i;
+                    break;
+                }
+            }
+            return nod;
+        }
+
+        public static long ThirdGCD(long a, long b)
+        {
+
+            long nod = 1L;
+
+            if (a > b)
+            {
+                long tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            while (a > 1L && b > 1L)
+            {
+                for (long i = 2; i <= a; i++)
+                {
+                    if (a % i == 0 && b % i == 0)
+                    {
+                        nod *= i;
+                        a /= i;
+                        b /= i;
+                        break;
+                    }
+
+                    if (a % i == 0)
+                    {
+                        a /= i;
+                        break;
+                    }
+
+                    if (b % i == 0)
+                    {
+                        b /= i;
+                        break;
+                    }
+                }
+            }
+            return nod;
+        }
+
+        public static long EuqlidGCDRec(long a, long b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+
+            if (a > b)
+            {
+                long tmp = a;
+                a = b;
+                b = tmp;
+            }
+            return EuqlidGCDRec(a, b - a);
+        }
+    }
+}
--- Task_Multithreading/Program.cs
+++ Task_Multithreading/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Task_Multithreading
 {
     class Program
     {
+        static void MyThread1()
+        {
+            //Console.WriteLine("/// First algorithm ///");
+            long firstGCD = GCDComputer.FirstGCD(65, 74);
+            Console.WriteLine("GCD = {0}", firstGCD);
+            //Console.WriteLine("///////////////////////////////////////////////////////////////////////");
+        }
+
+        static void MyThread2()
+        {
+            //Console.WriteLine("/// Second algorithm ///");
+            long secondGCD = GCDComputer.SecondGCD(32, 78);
+            Console.WriteLine("GCD = {0}", secondGCD);
+        }
+
+        static void MyThread3()
+        {
+            //Console.WriteLine("/// Third algorithm ///");
+            long thirdGCD = GCDComputer.ThirdGCD(25, 45);
+            Console.WriteLine("GCD = {0}", thirdGCD);
+        }
+
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+
+            Thread thread1 = new Thread(MyThread1);
+            Thread thread2 = new Thread(MyThread2);
+            Thread thread3 = new Thread(MyThread3);
+
+            thread1.Priority = ThreadPriority.Highest;
+            thread2.Priority = ThreadPriority.AboveNormal;
+            thread3.Priority = ThreadPriority.Highest;
+
+            stopwatch.Start();
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            //Console.WriteLine("/// Euqlid algorithm ///");
+            long euqlidGCD = GCDComputer.EuqlidGCDRec(63, 77);
+            Console.WriteLine("GCD = {0}", euqlidGCD);
+
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed = {0}", stopwatch.Elapsed);
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.ReadKey();
         }
     }
 }
